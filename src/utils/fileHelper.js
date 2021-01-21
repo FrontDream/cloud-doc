4fd96a613423fe110ae0fa87e99e2259df211489
qiandingwei <qiandingwei@longfor.com>
2021-01-21 15:26:26 +0800

    feat:新增文件帮助类

---
+++ src/utils/fileHelper.js
@@ -0,0 +1,21 @@
+const fs = window.require('fs').promises
+const path = window.require('path')
+
+const fileHelper = {
+    readFile: (path)=>{
+        return fs.readFile(path,{
+            encoding: 'utf-8'
+        })
+    },
+    writeFile: (path, content)=>{
+        return fs.writeFile(path, content,{encoding: 'utf-8'})
+    },
+    renameFile: (path, newPath)=>{
+        return fs.rename(path,newPath)
+    },
+    deleteFile: (path)=>{
+        return fs.unlink(path)
+    }
+}
+
+export default fileHelper
