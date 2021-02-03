f2b12943b4c9bf4d9de6be1df11d50556677d5ff
qiandingwei <qiandingwei@longfor.com>
2021-02-03 21:15:41 +0800

    comment: 搜索组件添加注释

--- src/components/FileSearch/index.js
+++ src/components/FileSearch/index.js
@@ -1,32 +1,40 @@
 import React, { useState, useEffect, useRef } from "react";
+import PropTypes from 'prop-types'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch, faTimes} from '@fortawesome/fontawesome-free-solid';
-import PropTypes from 'prop-types'
 import { useKeyPress } from '../../hooks'
 import './index.css'
 
 
 const FileSearch = ({ title, onFileSearch})=>{
+    // 输入框的状况
     const [inputActive, setInputActive] = useState(false)
+    // 用户输入的值
     const [value, setValue] = useState('')
+    // 监听回车键是否被按下
     const enterPressed = useKeyPress(13)
+    // 监听esc键是否被按下
     const esePressed = useKeyPress(27)
 
     let node = useRef(null)
 
     useEffect(()=>{
+        // 输入状态下，按下回车键
         if(enterPressed && inputActive){
             onFileSearch(value)
         }
+        // 在输入的状态下，按下esc键
         if(esePressed && inputActive){
             closeSearch()
         }
     })
+    // 当切换搜索时，当为输入框时，焦点聚焦
     useEffect(()=>{
         if(inputActive){
             node.current.focus()
         }
     }, [inputActive])
+    // 退出搜索，切换输入框，设置值为空，并将空值传递出去
     const closeSearch = ()=>{
         setInputActive(false)
         setValue('')
--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 
+/**
+ * 传入键盘码，返回当前按下的是否是传入的码所对应的键
+ * @param targetKeyCode
+ * @returns {boolean}
+ */
 const useKeyPress = (targetKeyCode)=>{
     const [keyPress,setKeyPress]= useState(false)
     const keyDownHandler = ({keyCode})=>{
