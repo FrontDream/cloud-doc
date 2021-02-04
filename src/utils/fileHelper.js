afa2e72040ebc69787eb5ba90a32a2e21674cc1c
qiandingwei <qiandingwei@longfor.com>
2021-02-04 09:19:16 +0800

    comment: fileHelper添加注释

--- src/utils/fileHelper.js
+++ src/utils/fileHelper.js
@@ -1,18 +1,21 @@
 const fs = window.require('fs').promises
-const path = window.require('path')
 
 const fileHelper = {
+    // 读取
     readFile: (path)=>{
         return fs.readFile(path,{
             encoding: 'utf-8'
         })
     },
+    // 写文件
     writeFile: (path, content)=>{
         return fs.writeFile(path, content,{encoding: 'utf-8'})
     },
+    // 重明命名文件
     renameFile: (path, newPath)=>{
         return fs.rename(path,newPath)
     },
+    // 删除文件
     deleteFile: (path)=>{
         return fs.unlink(path)
     }
